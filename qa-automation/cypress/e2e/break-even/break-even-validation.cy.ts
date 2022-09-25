b4ec1308a326a3e78dc83eeb7082c59c7eb6696d
Michael McGee <dineromike@yahoo.com>
2022-09-25 11:14:03 -0500

    added cypress, typescript for cypress and added basic happy path test

---
+++ qa-automation/cypress/e2e/break-even/break-even-validation.cy.ts
@@ -0,0 +1,85 @@
+describe('break-even validation', () => {
+
+  before(() => {
+    cy.visit('/break-even');
+  })
+
+  describe('revenue per unit', () => {
+
+    beforeEach(()=>{
+      cy.getDataTestId('revenue-per-unit').as('input');
+    })
+
+    it('should exist', () => {
+      cy.get('@input').should('exist');
+    })
+
+    it('should have error when clear and blur', () => {
+      cy.get('@input').clear().blur();
+      cy.get('@input').parent().should('have.class', 'has-error');
+    })
+
+    it('should not have error when 2 ', () => {
+      cy.get('@input').type('2');
+      cy.get('@input').parent().should('not.have.class', 'has-error');
+    })
+
+  });
+
+  describe('variableExpense', () => {
+
+    beforeEach(()=>{
+      cy.getDataTestId('variable-expense').as('input');
+    })
+
+    it('should exist', () => {
+      cy.get('@input').should('exist');
+    })
+
+    it('should have error when clear and blur', () => {
+      cy.get('@input').clear().blur();
+      cy.get('@input').parent().should('have.class', 'has-error');
+    })    
+
+    it('should have error when 2 greater than revenue ', () => {
+      cy.get('@input').clear().type('2');
+      cy.get('@input').parent().should('have.class', 'has-error');
+    })
+
+    it('should not have error when 1 ', () => {
+      cy.get('@input').clear().type('1');
+      cy.get('@input').parent().should('not.have.class', 'has-error');
+    })
+  });
+
+  describe('fixed expense', () => {
+
+    beforeEach(()=>{
+      cy.getDataTestId('fixed-expense').as('input');
+    })
+
+    it('should exist', () => {
+      cy.get('@input').should('exist');
+    })
+
+    it('should have error when clear and blur', () => {
+      cy.get('@input').clear().blur();
+      cy.get('@input').parent().should('have.class', 'has-error');
+    })
+
+    it('should not have error when 2 ', () => {
+      cy.get('@input').type('2');
+      cy.get('@input').parent().should('not.have.class', 'has-error');
+    })
+
+  });
+
+
+  
+
+  it('calulate button should exist', () => {
+    cy.getDataTestId('calculate-break-even').should('exist');
+  })
+
+
+})
