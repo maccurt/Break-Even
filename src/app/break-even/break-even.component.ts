51e03d78161f774a480cab3cc901b17dbbe410f2
Michael McGee <dineromike@yahoo.com>
2022-09-25 10:28:03 -0500

    added validator

--- src/app/break-even/break-even.component.ts
+++ src/app/break-even/break-even.component.ts
@@ -1,16 +1,18 @@
 import { UnitService } from './../unit.service';
-import { Component, OnInit } from '@angular/core';
-import { FormBuilder, FormControl, FormGroup } from '@angular/forms';
+import { Component } from '@angular/core';
+import { AbstractControl, FormBuilder, FormControl, FormGroup, ValidationErrors, Validators } from '@angular/forms';
+import { revenueVariableExpenseValidator } from '../validators/revenue-variable-expense.validator';
 
 @Component({
   selector: 'app-break-even',
   templateUrl: './break-even.component.html',
   styleUrls: ['./break-even.component.scss']
 })
-export class BreakEvenComponent implements OnInit {
+export class BreakEvenComponent {
 
   unitsToBreakEven?: number;
-
+  showErrors = false;
+  //form set up
   formGroup: FormGroup<{
     revenuePerUnit: FormControl<number>;
     variableExpense: FormControl<number>;
@@ -21,24 +23,36 @@ export class BreakEvenComponent implements OnInit {
     private unitService: UnitService) {
 
     this.formGroup = this.formBuilder.group({
-      revenuePerUnit: new FormControl(0, { nonNullable: true }),
-      variableExpense: new FormControl(0, { nonNullable: true }),
-      fixedExpense: new FormControl(0, { nonNullable: true })
+      revenuePerUnit: new FormControl(0, { nonNullable: true, validators: [Validators.required, Validators.min(1)] }),
+      //variable expense
+      variableExpense: new FormControl(0, {
+        nonNullable: true,
+        validators: [Validators.required, Validators.min(1)]
+      }),
+
+      fixedExpense: new FormControl(0, { nonNullable: true, validators: [Validators.required, Validators.min(1)] })
     });
+
+    this.formGroup.addValidators(revenueVariableExpenseValidator())
   }
 
-  ngOnInit(): void {
+  isInvalid = (control: AbstractControl): boolean => {
+    return (control.touched && control.invalid) || (control.invalid && this.showErrors);
+  }
+
+  isVariableExpenseValid = (control: AbstractControl): boolean => {
+
+    return this.isInvalid(control) ||
+      (control.touched && this.formGroup.errors && this.formGroup.errors['variableExpenseError']);
 
   }
 
   submit = () => {
-
     if (this.formGroup.valid) {
       let rpu = this.formGroup.value.revenuePerUnit!;
       let ve = this.formGroup.value.variableExpense!;
       let fe = this.formGroup.value.fixedExpense!;
       this.unitsToBreakEven = this.unitService.breakEvenUnits(rpu, ve, fe);
-
     }
   }
 }
