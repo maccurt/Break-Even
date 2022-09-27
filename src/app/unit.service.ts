8de3277b27a83b8c1fd37a9e015f9d249cf5a832
Michael McGee <dineromike@yahoo.com>
2022-09-27 13:00:13 -0500

    added unit test

--- src/app/unit.service.ts
+++ src/app/unit.service.ts
@@ -8,24 +8,27 @@ export class UnitService {
 
   constructor() { }
 
-  breakEvenUnitIncomeStatement = (revenuePerUnit: number, variableExpensePerUnit: number, fixedExpense: number): UnitIncomeStatement => {
+  //TODO move to math service
+  round = (value: number, precision: number = 2): number => {
+    const decimals = Math.pow(10, precision);
+    return Math.round(value * decimals) / decimals;
+  }
+
+  breakEvenUnitIncomeStatement = (revenuePerUnit: number, variableExpensePerUnit: number,
+    fixedExpense: number): UnitIncomeStatement => {
 
     const contributionMargin = this.contributionMargin(revenuePerUnit, variableExpensePerUnit);
     let units = this.breakEvenUnits(revenuePerUnit, variableExpensePerUnit, fixedExpense);
-
     const statement = new UnitIncomeStatement();
-    statement.revenue = units * revenuePerUnit;
-    statement.variableExpense = units * variableExpensePerUnit;
+    statement.revenue = this.round(units * revenuePerUnit);
+    statement.variableExpense = this.round(units * variableExpensePerUnit);
     statement.fixedExpense = fixedExpense;
-    statement.expenseTotal = statement.variableExpense + statement.fixedExpense;
-    statement.grossProfit = statement.revenue - statement.expenseTotal;
+    statement.expenseTotal = this.round(statement.variableExpense + statement.fixedExpense, 2)
+    statement.grossProfit = this.round(statement.revenue - statement.expenseTotal);    
     statement.contributionMargin = contributionMargin;
     statement.units = units;
-
     return statement;
   };
-
-
 
   breakEvenUnits = (revenuePerUnit: number, variableExpensePerUnit: number, fixedCost: number): number => {
     const contributionMargin = this.contributionMargin(revenuePerUnit, variableExpensePerUnit);
