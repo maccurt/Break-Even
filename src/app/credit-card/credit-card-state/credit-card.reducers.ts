0f06c789f4826bd51bd04c61f5614ce7964b5e8e
Michael McGee <dineromike@yahoo.com>
2022-12-01 07:43:55 -0600

    added some state to initial state for practice

--- src/app/credit-card/credit-card-state/credit-card.reducers.ts
+++ src/app/credit-card/credit-card-state/credit-card.reducers.ts
@@ -3,15 +3,22 @@ import { createReducer } from "@ngrx/store";
 
 export const creditCardFeatureKey = 'creditCard';
 export class CreditCardInput {
-    id!: string;
-}
+    id!: number;
+    interesRate!: number;
+};
+
 export interface CreditCardState extends EntityState<CreditCardInput> {
-}
+    creditCardInput: CreditCardInput
+};
 
 export const creditCardAdapter = createEntityAdapter<CreditCardInput>({
 });
 
 export const creditCardInitialState = creditCardAdapter.getInitialState({
+    creditCardInput: {
+        id: 1,
+        interesRate: 19.34 
+    }
 });
 
 export const creditCardReducer = createReducer<CreditCardState>(
