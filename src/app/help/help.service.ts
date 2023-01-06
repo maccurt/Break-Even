60549ae3fc6e5fd7e42599d07faa73831af33fcd
Michael McGee <dineromike@yahoo.com>
2023-01-06 17:06:23 -0600

    re-factored the front page to simplify

--- src/app/help/help.service.ts
+++ src/app/help/help.service.ts
@@ -173,7 +173,7 @@ export class HelpService {
     this.timeToPayOfDebt.icon = icons.time;
     this.timeSavedByPayingExtra.icon = icons.time;
     this.timeSavedFixedPayment.icon = icons.time;
-    this.creditCardDevil.icon = icons.creditCardDevil;
+    this.creditCardDevil.icon = icons.skull;
     this.minimumPaymentTrap.icon = icons.trap;
     this.creditCardInterestSavings.icon = icons.interestSaved;
     this.interestSavedByPayingExtra.icon = icons.interestSaved;
@@ -189,13 +189,9 @@ export class HelpService {
     this.creditCardInterestRate.hint = "Find your interest rate on your credit card bill.";
     this.creditCardCalculatorInstruction1.icon = IconService.creditCardInstruction1;
     this.creditCardLinkButton.icon = icons.creditCard;
-
-    this.creditCardBalance1.icon = icons.creditCard;
-    this.creditCardDevil.icon = icons.sackDollar;
+    this.creditCardBalance1.icon = icons.creditCard;    
     this.interestRate.icon = icons.interestRate;
     this.fixedPayment.icon = icons.commentDollar;
-
     this.minPaymentCalculation.icon = icons.calculator;
-
   }
 }
